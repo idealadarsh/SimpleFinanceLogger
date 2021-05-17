bf5f04427ccf8e8ed3bfee06b0a430a81996b6dd
Adarsh Kumar <adarsh@cvision-ai.com>
2021-05-18 00:15:21 +0530

    Initial commit

---
+++ public/main.js
@@ -0,0 +1,22 @@
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+const form = document.querySelector('.new-item-form');
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let values = [tofrom.value, details.value, amount.valueAsNumber];
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(...values);
+    }
+    else {
+        doc = new Payment(...values);
+    }
+    list.render(doc, type.value, 'end');
+});
